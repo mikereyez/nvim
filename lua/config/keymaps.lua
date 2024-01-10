483d1f564cb45836ef1f8204e11c71539b236f4e
Mike Koopman <mikekoopman@reyez.nl>
2024-01-10 17:52:38 +0100

    Added keymap for going up and down half a page with zz connected

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<c-d>", "<c-d>zz", { desc = "Go down half a page" })
+vim.keymap.set("n", "<c-u>", "<c-u>zz", { desc = "Go up half a page" })
